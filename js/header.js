ed5a479b95d979bda64d70f0ad2c4ffc2c27ccaf
Ravishankar Rajagopalan <vioravis@Ravishankars-MacBook-Pro.local>
2019-06-27 23:16:05 +0800

    Added functionalities for Signup, Signin, Create Posts and All Posts Buttons

--- js/header.js
+++ js/header.js
@@ -1,11 +1,12 @@
-// Get the modal
-var modal = document.getElementById("myModal");
+// Modal for Signup
+var modal = document.getElementById("signup-modal");
 
 // Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+var btn = document.getElementById("signup-button");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
+
 
 // When the user clicks the button, open the modal 
 btn.onclick = function() {
@@ -23,3 +24,30 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
+
+// Modal for Signin
+var modal1 = document.getElementById("signin-modal");
+
+// Get the button that opens the modal
+var btn1 = document.getElementById("signin-button");
+
+// Get the <span> element that closes the modal
+var span1 = document.getElementsByClassName("close")[1];
+
+// When the user clicks the button, open the modal 
+btn1.onclick = function() {
+  modal1.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span1.onclick = function() {
+  modal1.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal1) {
+    modal1.style.display = "none";
+  }
+}
+
