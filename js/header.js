901ce9b8ffe15dbb91c07968829d18e29c2b86c2
Ravishankar Rajagopalan <vioravis@Ravishankars-MacBook-Pro.local>
2019-06-29 15:00:03 +0800

    Added CSS styling

--- js/header.js
+++ js/header.js
@@ -13,6 +13,10 @@ btn.onclick = function() {
   modal.style.display = "block";
 }
 
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
