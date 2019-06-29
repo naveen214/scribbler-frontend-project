eabbe37510d316d089108e969cfa8d7c4573001a
krishnapotla” <krishnachaitanya.potla@gmail.com>
2019-06-29 15:34:33 +0530

    added delete event and other styles

--- js/bloglist.js
+++ js/bloglist.js
@@ -2,3 +2,10 @@
 document.getElementsByClassName("post")[
   document.getElementsByClassName("post").length - 1
 ].style.left = "25%";
+var delModal = document.getElementById("deleteModal");
+function showDeleteModal() {
+  delModal.style.display = "block";
+}
+function hideDeleteModal() {
+  delModal.style.display = "none";
+}
